e3731b9751af0aa142838685d4868d2f92249a7c
Rohith Goli <rohithgoli396@gmail.com>
2024-02-10 15:35:17 +0530

    feat: added endpoints returning Bean, accepting PathVariables

--- src/main/java/com/rrk/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rrk/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rrk.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,16 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello world");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(
+				String.format("Hello World, %s", name));
 	}
 }
