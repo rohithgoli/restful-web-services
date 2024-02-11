15543e99e2344ee8b4156729885def71d6fbd9e9
Rohith Goli <rohithgoli396@gmail.com>
2024-02-11 19:23:49 +0530

    feat: added i18n support for helloWorldController with MessageResource, messages.properties

--- src/main/java/com/rrk/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rrk/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.rrk.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
@@ -21,5 +31,12 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(
 				String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+//		return "Hello World V2";
 	}
 }
--- src/main/java/com/rrk/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/rrk/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -36,6 +36,6 @@ public class UserDaoService {
 	
 	public void deleteById(int id) {
 		Predicate<? super User> predicate = user -> user.getId().equals(id);
-		users.removeIf(predicate );
+		users.removeIf(predicate);
 	}
 }
