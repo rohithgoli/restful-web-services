ee37d375632754ae07dc2490554d22e721461496
Rohith Goli <rohithgoli396@gmail.com>
2024-02-12 10:33:00 +0530

    feat: added Request Parameter versioning for REST API

--- src/main/java/com/rrk/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/rrk/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -15,4 +15,14 @@ public class VersioningPersonController {
 	public PersonV2 getSecondVersionOfPerson() {
 		return new PersonV2(new Name("James", "Bond"));
 	}
+	
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getFirstVersionOfPersonRequestParameter() {
+		return new PersonV1("James Bond");
+	}
+	
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 getSecondVersionOfPersonRequestParameter() {
+		return new PersonV2(new Name("James", "Bond"));
+	}
 }
