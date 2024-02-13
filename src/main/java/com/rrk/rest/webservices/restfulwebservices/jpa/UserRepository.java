acdae2fea652e8b0dc4773095bec9a9564d85c6f
Rohith Goli <rohithgoli396@gmail.com>
2024-02-13 11:00:57 +0530

    feat: added usage of H2 Database with JPA and Hibernate

---
+++ src/main/java/com/rrk/rest/webservices/restfulwebservices/jpa/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rrk.rest.webservices.restfulwebservices.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rrk.rest.webservices.restfulwebservices.user.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
---
+++ src/main/java/com/rrk/rest/webservices/restfulwebservices/user/UserJpaResource.java
@@ -0,0 +1,67 @@
+package com.rrk.rest.webservices.restfulwebservices.user;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.rrk.rest.webservices.restfulwebservices.jpa.UserRepository;
+
+import jakarta.validation.Valid;
+
+@RestController	
+public class UserJpaResource {
+	
+	private UserRepository repository;
+	
+	public UserJpaResource(UserRepository repository) {
+		this.repository = repository;
+	}
+	
+	@GetMapping(path = "/jpa/users")
+	public List<User> retrieveAllUsers() {
+		return repository.findAll();
+	}
+	
+	@GetMapping(path = "/jpa/users/{id}")
+	public EntityModel<User> retrieveUserById(@PathVariable Integer id) {
+		Optional<User> user = repository.findById(id);
+		if (user.isEmpty()) 
+			throw new UserNotFoundException("id:"+id);
+		
+		EntityModel<User> entityModel = EntityModel.of(user.get());
+		
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		
+		return entityModel;
+	}
+	
+	@PostMapping(path = "/jpa/users")
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+		User savedUser = repository.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+						.path("/{id}")
+						.buildAndExpand(savedUser.getId())
+						.toUri();
+		return ResponseEntity.created(location).build();
+	}
+	
+	@DeleteMapping(path = "/jpa/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		repository.deleteById(id);
+	}
+}
